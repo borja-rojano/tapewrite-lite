312fc7359b51996b21057b3b2583295860bfb66b
borja-rojano <borja@tapewrite.com>
2017-01-01 18:54:58 +0100

    Image visible on Surge

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/src/index.html');
 });
 
+
+
 app.listen(3000, function () {
     console.log('Listening on port 3000...');
 });
