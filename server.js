ece51adeefdcfbfdfb47704607a531c0ad44d566
borja-rojano <borja@tapewrite.com>
2017-01-01 18:57:58 +0100

    Middleware successful
    Express can see the /src/assets file when referenced in the html as
    /assets

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express');
 
 var app = express();
 
+app.use('/assets', express.static(process.cwd()+'/src/assets'))
+
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/src/index.html');
 });
-
-
 
 app.listen(3000, function () {
     console.log('Listening on port 3000...');
